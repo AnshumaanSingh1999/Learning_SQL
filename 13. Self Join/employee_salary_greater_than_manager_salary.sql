0738b97548e7bfb8d0a090aceb6c06712b87f77f
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-02-27 22:55:27 +0530

    changes

--- 13. Self Join/employee_salary_greater_than_manager_salary.sql
+++ 13. Self Join/employee_salary_greater_than_manager_salary.sql
@@ -1,3 +1,3 @@
-SELECT emp.EID, emp.Name, emp.Salary, emp.ManagerID, mngr.Name AS "Manager Name" 
+SELECT emp.EID, emp.Name, emp.Salary, emp.ManagerID, mngr.Name AS "Manager Name", mngr.Salary AS "Manager Salary" 
 FROM employees emp JOIN employees mngr 
 ON emp.ManagerID=mngr.EID WHERE emp.Salary>mngr.Salary;
